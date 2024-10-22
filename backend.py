706808e9b12272a150a459ff895fdbe6503e5327
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-22 17:47:08 +0530

    Added

---
+++ backend.py
@@ -0,0 +1,7 @@
+API_KEY = ""
+
+def get_data(place, forecast_days,kind):
+    url = "api.openweathermap.org / data / 2.5 / forecast?id ={cityID} & appid = {APIkey}"
+
+
+    return data
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:")
 days = st.slider("Forecat Days",min_value=1,max_value=5,
@@ -7,13 +8,15 @@ days = st.slider("Forecat Days",min_value=1,max_value=5,
 option = st.selectbox("select data to view",
                       ("Temperature","sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
-def get_data(days):
-    dates = ["2022-25-10","2022-26-10","2022-27-10"]
-    temperature = [10,11,15]
-    temperature = [days * i for i in temperature]
-    return dates,temperature
 
-d, t =get_data(days)
+d, t = get_data(place,days, option)
+# def get_data(days):
+#     dates = ["2022-25-10","2022-26-10","2022-27-10"]
+#     temperature = [10,11,15]
+#     temperature = [days * i for i in temperature]
+#     return dates,temperature
+
+# d, t =get_data(days)
 
 figure = px.line(x =d,y=t,labels={"x":"Date","y":"Temperature (C)"})
 st.plotly_chart(figure)
